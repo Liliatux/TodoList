7f2fa202a68346c88e277f7ab1d198cd188a861b
Lilia <geertszelia@gmail.com>
2016-10-11 12:27:28 +0200

    voir les taches faites ou pas fini, début du filtre

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	"use strict";
 
+	var compteur = 0;
+
 	function newlist(a){
-		$("ul").append('<div id="list"><li><input id="check" type="checkbox"><label>'+a+'</label></li></div>');		
+		compteur++;
+		$("ul").append('<li><div id="list'+compteur +'"><input class="check" data-box="list'+compteur +'" type="checkbox"><label>'+ a +'</label></div></li>');	
 	}
 
 	function newtache(){
@@ -10,16 +13,36 @@ $(document).ready(function(){
 		newlist(input);	
 
 		function tachefaite(){
-			var tache = $("#check").is(":checked");
+			var idCheck = $(this).data('box');
+			var tache = $(this).is(":checked");
 			if(tache){
-				$("#list").css("background", "green");
-			} else{
-				$("#list").css("background", "white");
+				$('#'+idCheck).css("background", "green");
+				
+			} if(!tache){
+				$('#'+idCheck).css("background", "white");
+		
 			}
+			function filtre(){
+				var all = $("#all").is(":checked");
+				var fait = $('#fait').is(":checked");
+				var pasFait = $('#pasFait').is(':checked');
+				
+				if(all){
+				   ('li').show();
+				} if(fait){
+					$('#'+idCheck).parent('li').show();
+					$('#'+idCheck).parent('li').hide();
+				} if(pasFait){
+					$('#'+idCheck).parent('li').hide();
+				}
+				console.log("coucou");
+			}
+				$('#all').click(filtre);
+				$('#fait').click(filtre);
+				$("#pasFait").click(filtre);
 		}
 
-		$("#check").click(tachefaite);
-
+		$(".check").click(tachefaite);
 	}
 
 	$('#addList').click(newtache);
